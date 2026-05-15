6a2aa541d5d363c457322169c59544599ab498b3
prijenbalar <prijenbalar73@gmail.com>
2026-05-15 12:45:02 +0530

    Isolate telemetry_data components

--- cv_basics/draw_lines.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-image = cv2.imread("images/mike.jpg")
-image = cv2.resize(image,(500,500))
-cv2.line(image,(190,100),(310,103),(0,0,0),2)
-cv2.imshow("mike",image)
-cv2.waitKey(0)
--- cv_basics/draw_ractangle.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-image = cv2.imread("images/mike.jpg")
-image = cv2.resize(image,(500,500))
-cv2.rectangle(image,(170,95),(320,220),(0,0,0),2)
-cv2.imshow("mike",image)
-cv2.waitKey(0)
